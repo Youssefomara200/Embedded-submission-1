932dbb218bb234b32395bf96aa0e87321e5c125d
Youssefomara200 <117158066+Youssefomara200@users.noreply.github.com>
2022-11-03 15:58:45 +0200

    Add files via upload

---
+++ YousefApp/dio.h
@@ -0,0 +1,3 @@
+void Dio_SetPinDirection(unsigned char DDRX,unsigned char Pin,unsigned char Direction);
+void Dio_SetPortDirection(unsigned char PORTX,unsigned char Pin,unsigned char Direction);
+unsigned char Dio_GetPinState(unsigned char Port,unsigned char pin);
